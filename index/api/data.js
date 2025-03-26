f4fafd63c97bb26f2680cc41210bb5b37a3220a6
Sidhanth Bibi <sidhanthbibi@gmail.com>
2025-03-26 18:24:28 +0530

    ver. 3.5

--- index/api/data.js
+++ index/api/data.js
@@ -1,9 +1,7 @@
 // index/api/data.js
-
-import { latestData } from './update.js';
+import { latestData } from './update.js';  // Import the latestData from update.js
 
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Optional if accessed from other domains
-  res.status(200).json(latestData);
+  res.status(200).json(latestData);  // Serve the latest stored data
 }
