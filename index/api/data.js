fabc8fd3f6d20705a9f224d886693cc454bbd036
Sidhanth Bibi <sidhanthbibi@gmail.com>
2025-03-26 18:11:28 +0530

    ver. 3.0

--- index/api/data.js
+++ index/api/data.js
@@ -1,7 +1,9 @@
 // index/api/data.js
 
-import { latestData } from './update';
+import { latestData } from './update.js';
 
 export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Optional if accessed from other domains
   res.status(200).json(latestData);
 }
